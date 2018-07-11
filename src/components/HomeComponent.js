9b71e64a9cabffd6d8d6dc071876a796f62deb99
Marla Foreman <marla294@gmail.com>
2018-07-11 15:42:22 -0500

    Setting up HomeComponent
    Putting my HTML and CSS from my mockup into the HomeComponent, which
    will be the landing page for the site.

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+class HomeComponent extends React.Component {
+	render() {
+		return (
+			<div className="wrapper">
+				<nav className="top">
+					<p className="sitename">Marla Foreman</p>
+					<ul className="menu-top">
+						<li>
+							<a href="">About</a>
+						</li>
+						<li>
+							<a href="">Blog</a>
+						</li>
+						<li>
+							<a href="">Gallery</a>
+						</li>
+						<li>
+							<a href="">Contact</a>
+						</li>
+					</ul>
+				</nav>
+				<section className="about">
+					<img
+						className="pic"
+						src={require("../Assets/Pictures/SelfPortraits/20180607_SagradaFamilia.jpg")}
+					/>
+					<p className="desc">
+						Hi, I'm Marla, a self-taught Web Developer currently
+						calling Omaha, Nebraska home.{" "}
+						<a href="">Read More...</a>
+					</p>
+				</section>
+				<section className="blog">
+					<h1>From the blog...</h1>
+					<div className="snippets">
+						<div className="snippet post1">
+							<h3>Post Title I</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur
+								adipisicing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua. Ut
+								enim ad minim veniam, quis nostrud exercitation
+								ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in
+								reprehenderit in voluptate velit esse cillum
+								dolore eu fugiat nulla pariatur. Excepteur sint
+								occaecat cupidatat non proident, sunt in culpa
+								qui officia deserunt mollit anim id est laborum.
+							</p>
+						</div>
+						<div className="snippet post2">
+							<h3>Post Title II</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur
+								adipisicing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua. Ut
+								enim ad minim veniam, quis nostrud exercitation
+								ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in
+								reprehenderit in voluptate velit esse cillum
+								dolore eu fugiat nulla pariatur. Excepteur sint
+								occaecat cupidatat non proident, sunt in culpa
+								qui officia deserunt mollit anim id est laborum.
+							</p>
+						</div>
+					</div>
+				</section>
+				<section className="contact">
+					<h1>Connect with me</h1>
+					<p className="email">marla294@gmail.com</p>
+					<p className="phone">312-576-4246</p>
+					<div className="social">
+						<p>LinkedIn, Twitter, Instagram, Github</p>
+					</div>
+				</section>
+			</div>
+		);
+	}
+}
+
+export default HomeComponent;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import { render } from "react-dom";
+import HomeComponent from "./components/HomeComponent";
+import "./css/Styles.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(<HomeComponent />, document.getElementById("root"));
