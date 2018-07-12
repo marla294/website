e544cf1045765e53fdd0b7840d82649eaa153c59
Marla Foreman <marla294@gmail.com>
2018-07-12 11:04:00 -0500

    Changed bottom section to "footer"

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -67,14 +67,14 @@ class HomeComponent extends React.Component {
 						</div>
 					</div>
 				</section>
-				<section className="contact">
+				<footer className="contact">
 					<h1>Connect with me</h1>
 					<p className="email">marla294@gmail.com</p>
 					<p className="phone">312-576-4246</p>
 					<div className="social">
 						<p>LinkedIn, Twitter, Instagram, Github</p>
 					</div>
-				</section>
+				</footer>
 			</div>
 		);
 	}
