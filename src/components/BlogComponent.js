7e04377d749ffd53ab30bfc5ee961fda56f3acfe
Marla Foreman <marla294@gmail.com>
2018-07-17 12:40:54 -0500

    Created PostSnippetComponent
    - Created component
    - Hooked it up to Blog Component
    
    This will be a component that displays data about blog posts to be used
    throughout the app.

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Snippet from "./PostSnippetComponent";
 
 class BlogComponent extends React.Component {
 	render() {
 		return (
 			<div className="wrapper">
 				<p>This is the blog component</p>
+				<Snippet />
 			</div>
 		);
 	}
---
+++ src/components/PostSnippetComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class PostSnippetComponent extends React.Component {
+	render() {
+		return <p>This is a post snippet</p>;
+	}
+}
+
+export default PostSnippetComponent;
