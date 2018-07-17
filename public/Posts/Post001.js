6e2b4b8f698b85101a495e87617f72b97239e387
Marla Foreman <marla294@gmail.com>
2018-07-17 16:43:16 -0500

    Handling post content
    Going to try injecting the post content into the post component

---
+++ public/Posts/Post001.js
@@ -0,0 +1,7 @@
+import react from "React";
+
+const post = () => {
+	return <p>Hi I'm a post</p>;
+};
+
+export default post;
--- src/blog-posts.js
+++ src/blog-posts.js
@@ -1,11 +1,11 @@
 /* Where all the high-level blog post information will go */
-
 const posts = {
 	post001: {
 		title: "When We Went To Barcelona",
 		date: "July 16, 2018",
 		postNum: 1,
-		headerImage: "/Images/Pictures/Spain/20180607_Flags.jpg"
+		headerImage: "/Images/Pictures/Spain/20180607_Flags.jpg",
+		content: "/Posts/Post001.js"
 	}
 };
 
