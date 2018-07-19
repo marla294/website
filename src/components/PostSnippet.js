780ae1b1246762e2ddbb2b0218d6528fe25cd602
Marla Foreman <marla294@gmail.com>
2018-07-19 14:56:56 -0500

    Styling post snippet

--- src/components/PostSnippet.js
+++ src/components/PostSnippet.js
@@ -5,15 +5,17 @@ class PostSnippet extends React.Component {
 	render() {
 		return (
 			<div className="snippet" onClick={this.goToPost}>
+				<div className="snippet_overlay">
+					<div className="snippet_details">
+						<h3 className="title">{this.props.post.title}</h3>
+						<p className="date">{this.props.post.date}</p>
+					</div>
+				</div>
 				<img
 					className="snippet_img"
 					src={`${this.props.post.headerImage}`}
 					alt=""
 				/>
-				<div className="snippet_details">
-					<h3 className="title">{this.props.post.title}</h3>
-					<p className="date">{this.props.post.date}</p>
-				</div>
 			</div>
 		);
 	}
