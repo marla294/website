365abaaf9f4a33edf2aec94fb59ee079f3eb22d1
Marla Foreman <marla294@gmail.com>
2018-07-26 15:56:52 -0500

    Post001

--- src/Posts/post001.js
+++ src/Posts/post001.js
@@ -4,114 +4,36 @@ class PostData extends React.Component {
 	render() {
 		return (
 			<div className="post_content">
-				<h4>&#191;Por Que Barcelona?</h4>
+				<h4>Hi.</h4>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-					Quis obcaecati facere voluptatibus iste impedit
-					reprehenderit. Incidunt quas ratione molestiae, eaque natus
-					alias magni sequi illo id ducimus nulla voluptas. Ducimus ab
-					porro mollitia et cupiditate, quam illo! Ullam debitis id,
-					aliquid quis sed tenetur enim consequuntur nemo laudantium
-					pariatur reprehenderit voluptates dicta nesciunt omnis,
-					illum alias atque tempore sit incidunt vero soluta fuga odio
-					facere? Ut laborum assumenda veritatis consectetur! Fugit,
-					nihil, sint. Iure sapiente, asperiores minima rem suscipit
-					labore, et dignissimos dolorem numquam quisquam error
-					temporibus iste? Animi molestiae similique quidem minima,
-					voluptatibus consequatur cumque saepe delectus. Alias,
-					nihil!
+					To be totally transparent, I don't really know what I'm
+					going to write about here. I have some ideas floating around
+					but, I made this thing mostly so that I could have a project
+					to do in React. I am not the biggest code-project idea
+					generator in the world, and that's why over the past year of
+					learning to code, this is my 3rd blog that I've built from
+					the ground up. Sure helped me get better at HTML and CSS
+					that way. The funny thing is, after I create the blog,
+					writing IN the blog is actually the hardest part.
 				</p>
-
-				<figure className="post_contentImg right">
-					<img
-						onClick={this.props.showFullImage}
-						src={require("../Assets/Pictures/SelfPortraits/20180607_Graffiti.jpg")}
-						alt="Marla in front of graffiti wall"
-					/>
-					<figcaption className="figcaption">
-						Standing in front of a cool wall
-					</figcaption>
-				</figure>
-
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-					Minima recusandae, suscipit. Labore, provident magni in
-					magnam est consequatur sint incidunt accusamus alias cumque,
-					nisi dicta neque eligendi recusandae dolorum, ab quisquam
-					quibusdam. Dignissimos natus repellat quae consequuntur
-					praesentium voluptas tempore fugiat quod expedita modi,
-					consequatur aut laboriosam culpa, provident sunt reiciendis
-					non officia vero totam pariatur corporis saepe officiis eum
-					deserunt! Ex, quasi. Rem, perspiciatis, assumenda. Quasi,
-					rerum sed, perspiciatis magni eaque natus magnam quis
-					tenetur dolorem, officiis reiciendis id, facilis quas ut
-					aliquam cupiditate tempora voluptate provident corporis
-					deleniti. Laudantium tempora voluptatem possimus ex error
-					enim, dolor obcaecati similique sequi! Minima vero atque
-					laborum sit cupiditate nesciunt odit soluta accusantium
-					aperiam, facilis accusamus, quo eveniet et necessitatibus at
-					nulla temporibus, cum eum hic quas nisi facere ipsam ipsum
-					provident. Deleniti molestiae ducimus, deserunt delectus
-					fugit dolore sit recusandae numquam quam harum vitae
-					debitis, tenetur laudantium illum eum ratione, asperiores
-					magni provident dolorum. Aperiam porro, soluta minima
-					similique expedita cupiditate fugiat voluptates autem libero
-					inventore voluptatibus unde, eius sunt, magni cum quidem
-					reiciendis. Aut optio, quidem, nisi eos temporibus repellat
-					quisquam animi. Quisquam voluptates, quibusdam est odio
-					deleniti quae impedit recusandae ipsum omnis amet placeat
-					nihil repellat ipsa vero illum tempora suscipit quis dicta
-					itaque, veniam reprehenderit, doloribus excepturi. Facere
-					saepe omnis laborum, incidunt quo asperiores. Dolorem
-					voluptatum architecto odio praesentium corporis voluptates
-					placeat eius commodi laudantium est, impedit harum ut
-					officiis et molestias sequi necessitatibus numquam velit.
-					Dolore, laudantium ut maxime sunt impedit vitae accusamus
-					quae dignissimos, aperiam veritatis.
+					I kind of want to keep it to one subject area. It seems like
+					it would get annoying if I talked about like, my personal
+					life in one post and then went and talked about development
+					stuff. But on the other hand, I kind of WANT to talk about
+					both, and I have pretty interesting stuff to talk about in
+					both areas... so we'll see.
 				</p>
-
-				<h4>What we did there</h4>
-
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-					Cumque obcaecati beatae nam, commodi aspernatur animi
-					quibusdam sequi esse, laboriosam culpa, adipisci suscipit
-					error natus doloribus aperiam aliquam libero expedita optio
-					voluptatibus vitae sunt ipsum quia voluptatem laborum.
-					Suscipit quia amet, minus. Nesciunt, ipsam impedit accusamus
-					cumque maiores, vitae corporis consequuntur!
+					As per usual with me, I created this website from scratch.
+					It is built in ReactJS. In my humble opinion, there are a
+					few cool things in this website that I had to do to build
+					it, like lazy-loading components and CSS Grid, so if you're
+					interested, check it out - source code is on Github in the
+					"website" project. And I'm not done with it! There's a lot
+					more I want to do.
 				</p>
-
-				<figure className="post_contentImg left">
-					<img
-						onClick={this.props.showFullImage}
-						src={require("../Assets/Pictures/SelfPortraits/20180607_Graffiti.jpg")}
-						alt="Marla in front of graffiti wall"
-					/>
-					<figcaption className="figcaption">
-						Standing in front of a cool wall 2
-					</figcaption>
-				</figure>
-
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-					Recusandae aspernatur tempora, repellat placeat vel, ex
-					veritatis qui excepturi sed quibusdam voluptas nostrum
-					dolore aut! Et provident est, deleniti quo quas eaque
-					temporibus non obcaecati, harum aliquam voluptate recusandae
-					saepe, enim quidem repellendus porro officiis soluta. Error
-					explicabo eos dolores quia nobis recusandae veritatis
-					pariatur autem, ad omnis sed dignissimos adipisci quisquam
-					iure repellendus, nisi officia corporis, fugit nostrum
-					molestiae quam debitis. Iure eligendi debitis quia eum
-					commodi quis! Architecto optio sint quaerat accusamus est
-					consequatur ex eveniet, nemo laudantium obcaecati amet velit
-					voluptatum laboriosam suscipit nulla iure delectus iusto
-					autem, reprehenderit tempore neque nobis dolorem. Nulla
-					suscipit veniam necessitatibus nihil, sapiente ipsam. Esse
-					accusantium, ab, ipsam dolorum corporis illum nemo odit modi
-					officiis dignissimos explicabo.
-				</p>
+				<p>Anyway, if you're here, I'm glad to have you.</p>
 			</div>
 		);
 	}
--- src/blog-posts.js
+++ src/blog-posts.js
@@ -1,28 +1,12 @@
 /* Where all the high-level blog post information will go */
 const posts = {
 	Post001: {
-		title: "When We Went To Barcelona",
-		date: "July 16, 2018",
-		headerImage: "/Images/Pictures/Spain/20180607_Flags.jpg",
+		title: "About This Blog Thing...",
+		date: "July 26, 2018",
+		headerImage: "/Images/Pictures/Omaha/20180726_Office.jpg",
 		content: "/Posts/Post001.js",
-		categories: ["Travel", "Personal", "Trains"],
+		categories: ["Coding", "ReactJS"],
 		order: 1
-	},
-	Post002: {
-		title: "Trains... because, trains",
-		date: "July 20, 2018",
-		headerImage: "/Images/Pictures/Omaha/20180719_Trains.jpg",
-		content: "/Posts/Post002.js",
-		categories: ["Trains"],
-		order: 2
-	},
-	Post003: {
-		title: "Here's another one",
-		date: "July 20, 2018",
-		headerImage: "/Images/Pictures/Omaha/20180719_Trains.jpg",
-		content: "/Posts/Post003.js",
-		categories: ["Trains", "Random"],
-		order: 3
 	}
 };
 
