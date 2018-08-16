6f3f9909b9cd38ff6c9040e4f074eae9a674a3ea
Marla Foreman <marla294@gmail.com>
2018-08-16 11:32:04 -0500

    Turning PostSnippet into a stateless functional component

--- src/components/PostSnippet.js
+++ src/components/PostSnippet.js
@@ -2,34 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../css/PostSnippet.css";
 
-class PostSnippet extends React.Component {
-	/* Click Events */
+const goToPost = (post, push, event) => {
+	event.preventDefault();
+	const slugify = require("slugify");
+	const slug = slugify(post.title, { remove: /\./ });
+	push(`/Post/${slug}`);
+};
 
-	goToPost = event => {
-		event.preventDefault();
-		const slugify = require("slugify");
-		const slug = slugify(this.props.post.title, { remove: /\./ });
-		this.props.push(`/Post/${slug}`);
-	};
-
-	render() {
-		return (
-			<div className="snippet" onClick={this.goToPost}>
-				<div className="snippet_overlay">
-					<div className="snippet_details">
-						<h3 className="title">{this.props.post.title}</h3>
-						<p className="date">{this.props.post.date}</p>
-					</div>
-				</div>
-				<img
-					className="snippet_img"
-					src={`${this.props.post.headerImage}`}
-					alt=""
-				/>
+const PostSnippet = ({ post, push }) => (
+	<div
+		className="snippet"
+		onClick={event => {
+			goToPost(post, push, event);
+		}}
+	>
+		<div className="snippet_overlay">
+			<div className="snippet_details">
+				<h3 className="title">{post.title}</h3>
+				<p className="date">{post.date}</p>
 			</div>
-		);
-	}
-}
+		</div>
+		<img className="snippet_img" src={`${post.headerImage}`} alt="" />
+	</div>
+);
 
 PostSnippet.propTypes = {
 	post: PropTypes.object.isRequired
