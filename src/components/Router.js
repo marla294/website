be3ae23a87e83454d9038a2e3c250e2590b66d37
Marla Foreman <marla294@gmail.com>
2018-08-16 10:38:17 -0500

    Turning Router into a stateless functional component

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,33 +5,28 @@ import About from "./About";
 import Post from "./Post";
 import Blog from "./Blog";
 import Posts from "../blog-posts";
-// import Contact from "./Contact";
 
-class Router extends React.Component {
-	home = props => {
-		return <Home posts={Posts} {...props} />;
-	};
+const home = props => {
+	return <Home posts={Posts} {...props} />;
+};
 
-	post = props => {
-		return <Post posts={Posts} {...props} />;
-	};
+const post = props => {
+	return <Post posts={Posts} {...props} />;
+};
 
-	blog = props => {
-		return <Blog posts={Posts} {...props} />;
-	};
+const blog = props => {
+	return <Blog posts={Posts} {...props} />;
+};
 
-	render() {
-		return (
-			<BrowserRouter>
-				<Switch>
-					<Route exact path="/" render={this.home} />
-					<Route path="/About" component={About} />
-					<Route path="/Post/:Slug" render={this.post} />
-					<Route path="/Blog" render={this.blog} />
-				</Switch>
-			</BrowserRouter>
-		);
-	}
-}
+const Router = props => (
+	<BrowserRouter>
+		<Switch>
+			<Route exact path="/" render={home} />
+			<Route path="/About" component={About} />
+			<Route path="/Post/:Slug" render={post} />
+			<Route path="/Blog" render={blog} />
+		</Switch>
+	</BrowserRouter>
+);
 
 export default Router;
