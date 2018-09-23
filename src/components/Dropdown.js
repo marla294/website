0cc83271a7b744b749fbbf27df68bd8d3b1b15be
Marla Foreman <marla294@gmail.com>
2018-09-23 09:47:30 -0500

    Forgot the return on the render method for Dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,20 +2,25 @@ import React from "react";
 
 class Dropdown extends React.Component {
 	render() {
-		<div className="dropdown">
-			<button onClick={this.toggleCategoryDropdown} className="dropbtn">
-				Categories
-			</button>
-			<div
-				className={
-					this.state.showCategoryDropdown
-						? "dropdown-content show"
-						: "dropdown-content"
-				}
-			>
-				{this.renderCategories()}
+		return (
+			<div className="dropdown">
+				<button
+					onClick={this.toggleCategoryDropdown}
+					className="dropbtn"
+				>
+					Categories
+				</button>
+				<div
+					className={
+						this.state.showCategoryDropdown
+							? "dropdown-content show"
+							: "dropdown-content"
+					}
+				>
+					{this.renderCategories()}
+				</div>
 			</div>
-		</div>;
+		);
 	}
 }
 
