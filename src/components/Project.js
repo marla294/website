f6674ee78017c36dcba25ec5bca3e5aa2b340381
Marla Foreman <marla294@gmail.com>
2019-01-07 11:26:33 -0600

    Created Project component and added more styles to Projects.js

---
+++ src/components/Project.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Project = (props) => {
+	return <div>A Project</div>;
+}
+
+export default Project;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import TopNav from "./TopNav";
+import Project from "./Project";
+import { GlobalStyle } from "./GlobalStyles";
 
 const ProjectsWrapper = styled.div`
 	margin-top: var(--S06);
@@ -9,13 +11,34 @@ const ProjectsWrapper = styled.div`
 	justify-items: center;
 `;
 
+const ProjectsContent = styled.div`
+	margin-top: var(--S06);
+	display: grid;
+	grid-template-rows: repeat(auto-fit, auto);
+	grid-gap: 20px;
+	
+	h1 {
+		color: var(--Gray05);
+		padding-left: var(--S03);
+		font-size: var(--F06);
+	}
+
+	@media only screen and (min-width: 768px) {
+		margin-bottom: var(--S07);
+	}
+`;
+
 class Projects extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
 				<TopNav push={this.props.history.push} />
 				<ProjectsWrapper>
-					<div>Projects Page</div>
+					<ProjectsContent>
+						<h1>Projects Page</h1>
+						<Project />
+					</ProjectsContent>
+					<GlobalStyle />
 				</ProjectsWrapper>
 			</React.Fragment>
 		);
