0857a924df9e01e28c5433b293d5cb53ddb3b9a2
Marla Foreman <marla294@gmail.com>
2019-01-07 11:08:22 -0600

    Creating a Projects page

---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Projects extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<div>Projects Page</div>
+			</React.Fragment>
+		);
+	}
+}
+
+export default Projects;
