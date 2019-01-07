7836cb63186f6d4329ac20d78bd9d1b04dadb251
Marla Foreman <marla294@gmail.com>
2019-01-07 11:18:04 -0600

    High level styling on Projects page component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import TopNav from "./TopNav";
+
+const ProjectsWrapper = styled.div`
+	margin-top: var(--S06);
+	width: 100%;
+	display: grid;
+	justify-items: center;
+`;
 
 class Projects extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
-				<div>Projects Page</div>
+				<TopNav push={this.props.history.push} />
+				<ProjectsWrapper>
+					<div>Projects Page</div>
+				</ProjectsWrapper>
 			</React.Fragment>
 		);
 	}
