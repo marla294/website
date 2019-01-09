2a438b53067edebc255a8bf954d7d670afb75a64
Marla Foreman <marla294@gmail.com>
2019-01-09 11:11:26 -0600

    Post005

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -5,14 +5,17 @@ const Block = styled.code`
 	width: 100%;
 	background: ${props => props.theme.Gray01};
 	padding: ${props => props.theme.S04} ${props => props.theme.S04};
+	margin: ${props => props.theme.S03} 0;
+	white-space: pre-wrap;
 
-	font-size: ${props => props.theme.F03}
+	font-size: ${props => props.theme.F01}
 
 	@media only screen and (min-width: 768px) {
 		width: 768px;
 	}
 `;
 
+// Pass children as an array please
 export default class CodeBlock extends React.Component {
 	render() {
 		return (
