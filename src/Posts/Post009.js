e69c04132d2ff9357794b5d12ab12b70ea371efb
Marla Foreman <marla294@gmail.com>
2019-02-18 23:33:07 -0600

    Post009

---
+++ src/Posts/Post009.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const PostData = props => (
+	<React.Fragment>
+	
+	<p>(The header photo was taken by me, at my parent's cabin in Marquette Michigan)</p>
+
+<p>I gave up drinking (uh, alcohol) a little over a year and a half ago.  When I gave it up, it was a "thing," but it's been over a year now and it hasn't really been a "thing" for a while, until I started my new job a couple weeks ago.</p>
+
+<p>Don't get me wrong now, I love my job!  It's flipping awesome.  I'm still in a state of shock that I work there.  Their office is amazing, the people are super nice, and they go above and beyond to make their people happy.  And best of all I get to code in .NET all day long!  Oh yeah and I just remembered, I might have gotten paid a few days ago!  Yay money!  I am very happy with my new job.</p>
+
+<p>The only thing that has been a little challenging is the alcohol issue.  People comment on my not drinking, because drinking is a big part of their culture.  It's not just them, it's kind of a big part of the tech culture in general.  I'm totally fine with other people drinking.  It's fun to be around them!  I just feel kind of awkward sometimes because I don't know how to explain why I don't drink.  I haven't had to yet.  So I'm going to practice by putting it out on the interwebs.</p>
+
+<p>I had depression.  I dealt with it for a long time, since my mid-20s.  I say "had" because I don't feel I am depressed anymore, but it's probably still in there, waiting for me to slip up.  Up until I quit drinking, I was somewhat using alcohol as a coping mechanism for both depression and stress, instead of dealing with problems head on.  When something stressful would happen, I'd go and drink too much and forget about it.  This caused the issues in my life to snowball and snowball until they got out of control, because I never dealt with them effectively.</p>
+
+<p>Finally had the stereotypical "one night" where I went on a terrible (terrific?) bender, and the next day, felt awful.  It caused me to realize that 1) I really hate hangovers and 2) I have been numbing my problems by over-drinking.</p>
+
+<p>I am really bad at moderation.  I can't have "just one drink."  Like, come on, that requires super-human strength in certain situations.  I can't drink every once in a while.  Any of these things are going to lead me right back down the path to abusing it again.  So I decided to go cold turkey and give it up completely after that bender.</p>
+
+<p>Surprisingly, it wasn't that hard to give up.  There were a couple times in the first month where I had a bad day, and I noticed an urge to go to the grocery store and buy something, and I didn't do it.  Also it was a little difficult to manage the transition with my husband who still drinks.  For a few months I asked him to not have alcohol in the house because I didn't trust myself to not drink it, but now he can have it around.  I went on a cruise about 6 months in where everyone was drinking... honestly that was pretty tough!  I got through it by ordering a lot of virgin strawberry daiquiris.  But other than those few things it's been cake.</p>
+
+<p>At the beginning, I really thought I would be bored out of my mind and that life wouldn't be fun anymore.  A year and a half in I can tell you - totally not the case!  I like my life a whole lot better now without drinking.  I get so much more done.  I definitely 100% for sure couldn't have done this self-taught coding thing if I was still drinking.  I'm way happier.  For some reason it's like, the mundane everyday things that I do have gotten funner.  I just feel so much better and like my life so much more now.</p>
+
+<p>The only alcohol I'll allow myself is if it's in food.  I had beer cheese soup in front of coworkers as I told them I don't drink which probably confused the hell out of them!  I don't think alcohol in food does anything, so I don't count it.</p>
+
+<p>I don't know if I'll ever pick up drinking again.  Maybe I will someday.  For sure, you can bet, if I make it to 90 and I am still sane, I'll be drinking, and smoking, and probably smoking crack too, because why the hell not.  But for now this is working for me, and I don't see it changing anytime soon.</p>
+
+	</React.Fragment>
+);
+
+export default PostData;
