f9d7cf4a7d939d6cd77f5f8e9d65d3d4788dcae4
marla294@gmail.com <marla294@gmail.com>
2021-01-23 10:34:25 -0600

    post019 fix for codeblock tab

--- src/Posts/Post019.js
+++ src/Posts/Post019.js
@@ -9,33 +9,33 @@ const PostData = props => (
 	<p>The first one is from a problem I looked at with a SQL query last week.  We were already unioning 2 tables that had similar info in a stored procedure.  It was really important to keep the tables in a certain order when spitting out the output.  The procedure sort of looked like this (this is pseudocode, don't @ me if it's not exactly syntactically correct):</p>
 
 	<CodeBlock>{`
-		select make, model
-		from (
-			select make, model, 0 as ordinal
-			from tableA
+select make, model
+from (
+	select make, model, 0 as ordinal
+	from tableA
 
-			union all
+	union all
 
-			select make, model, 1 as ordinal
-			from tableB
-		) makemodel
-		order by ordinal
+	select make, model, 1 as ordinal
+	from tableB
+) makemodel
+order by ordinal
 	`}</CodeBlock>
 
 	<p>There were duplicate make/models in both tables, and we wanted to remove them, while keeping the ordering the same.  First we tried just changing over to a union, instead of union all, which will act like a distinct and get rid of the dupes:</p>
 
 	<CodeBlock>{`
-		select make, model
-		from (
-			select make, model, 0 as ordinal
-			from tableA
+select make, model
+from (
+	select make, model, 0 as ordinal
+	from tableA
 
-			union
+	union
 
-			select make, model, 1 as ordinal
-			from tableB
-		) makemodel
-		order by ordinal
+	select make, model, 1 as ordinal
+	from tableB
+) makemodel
+order by ordinal
 	`}</CodeBlock>
 
 	<p>But, that didn't work because of the ordinal.  It got rid of the duplicates in each table individually, but if there were dupes between the 2 tables those were still there.</p>
@@ -43,19 +43,19 @@ const PostData = props => (
 	<p>After a lot of trial and error and research, here's what I arrived at that fixed the issue:</p>
 
 	<CodeBlock>{`
-		select make, model
-		from (
-			select make, model, 0 as ordinal
-			from tableA
+select make, model
+from (
+	select make, model, 0 as ordinal
+	from tableA
 
-			union
+	union
 
-			select tableB.make, tableB.model, 1 as ordinal
-			from tableB
-			left join tableA on tableB.make = tableA.make and tableB.model = tableB.model
-			where tableA.make is null
-		) makemodel
-		order by ordinal
+	select tableB.make, tableB.model, 1 as ordinal
+	from tableB
+	left join tableA on tableB.make = tableA.make and tableB.model = tableB.model
+	where tableA.make is null
+) makemodel
+order by ordinal
 	`}</CodeBlock>
 
 	<p>This takes the duplicate records between tableA and tableB out of tableB.  So now there are no duplicate records between the two tables, and we could still do our sorting by the ordinal.</p>
