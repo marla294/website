996a8d19ff2737617bb480a2fb0f3eb43aa18063
marla294@gmail.com <marla294@gmail.com>
2021-06-21 23:39:47 -0500

    Post 20

---
+++ src/Posts/Post020.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const PostData = props => (
+	<React.Fragment>
+
+	<p>Hi!  I'm going to just get right down to it - I'm pregnant!  I am currently 15 weeks along with a healthy baby girl!  She will be joining our family in mid-December of 2021!  We are so excited to meet her!  Our son Miles is going to be the best big brother!</p>
+
+	<p>I haven't posted in a while because, well, don't hate, but this blog is at the bottom of my priority list.  There are 2 things I care about in life - my family, and getting better at software development.  Everything else that I can fit in is gravy.  I could absolutely not care any less about "personal branding" or whatever - just typing that phrase makes me queasy.  It's an outlet.  I enjoy writing.  I post about things that I'm interested in that I think could also help others.  Also, my philosophy has changed where, I don't like to put out negativity into the world, and there have been a few times throughout the past 6 months where I've started a post, and then scrapped it, because it didn't feel right.  So that is why there's been a lot of silence here.</p>
+
+	<p>But I do have a couple of post ideas for things that I'd like to put up, and they aren't negative.  I'll just throw down the gauntlet now and say my first idea is, how do you continue to learn while you're a professional developer?  And what the hell, the second idea is, WHAT do you learn?  I have many thoughts on both of these things.</p>
+
+	<p>Well, I hope you are sufficiently shocked by this news, coming out of nowhere, because that was my intention!  My goal was to make at least one person fall off their chair and I hope I've done my job.  Who am I kidding, I don't need Google Analytics (which I purposefully do not have installed) to know that I probably have about 0.5 visitors per month!  Haha, take care!</p>
+
+	</React.Fragment>
+);
+
+export default PostData;
