ca00ea26b3e3d21a132c907a4bddfb28e8b10e10
Marla Foreman <marla294@gmail.com>
2022-02-21 16:13:31 -0600

    Update Home component to use Blog component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,76 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
-import TopNav from "./TopNav";
-import Snippet from "./PostSnippet";
-import { GlobalStyle } from "./GlobalStyles";
-
-const HomeWrapper = styled.div`
-	width: 100%
-	display: grid;
-	justify-items: center;
-`;
-
-const HomeContent = styled.div`
-	margin-top: var(--S06);
-	display: grid;
-	grid-template-rows: repeat(auto-fit, auto);
-	grid-gap: 20px;
-
-	h1 {
-		color: var(--Gray05);
-		padding-left: var(--S03);
-		font-size: var(--F06);
-	}
-
-	@media only screen and (min-width: 768px) {
-		margin-bottom: var(--S07);
-	}
-`;
-
-const BlogPosts = styled.div`
-	justify-self: center;
-	display: grid;
-	grid-template-columns: 1fr;
-	grid-gap: 40px;
-	justify-items: center;
-`;
+import Blog from "./Blog";
 
 class Home extends React.Component {
-	/* Render Functions */
-
-	renderPostSnippets = () => {
-		const displayKeys = Object.entries(this.props.posts)
-			.sort((a, b) => b[1].order - a[1].order)
-			.map(post => post[0])
-			.slice(0, 2);
-
-		return displayKeys.map(key => {
-			return (
-				<Snippet
-					key={key}
-					index={key}
-					post={this.props.posts[key]}
-					push={this.props.history.push}
-				/>
-			);
-		});
-	};
-
 	render() {
 		return (
-			<React.Fragment>
-				<TopNav push={this.props.history.push} />
-					<HomeWrapper>
-						<HomeContent>
-							<h1>Latest Blog Posts</h1>
-							<BlogPosts>
-								{this.renderPostSnippets()}
-							</BlogPosts>
-						</HomeContent>
-						<GlobalStyle />
-					</HomeWrapper>
-			</React.Fragment>
+			<Blog 
+				posts={this.props.posts} 
+				history={this.props.history}
+				amountOfPostsToDisplay={2}
+			></Blog>
 		);
 	}
 }
