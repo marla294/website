e309b446866b45174ec108d2d933c6f8d9bc4cbc
Marla Foreman <marla294@gmail.com>
2022-02-24 13:26:00 -0600

    Turning Router into a regular React component

--- src/components/Router.js
+++ src/components/Router.js
@@ -9,6 +9,7 @@ import Blog from "./Blog";
 import Posts from "../blog-posts";
 import Projects from './Projects';
 import ManageAbout from "./ManageAbout";
+import base from '../base';
 
 /* Render Functions */
 
@@ -28,19 +29,23 @@ const projects = props => {
 	return <Projects posts={Posts} {...props} />;
 };
 
-const Router = props => (
-	<ThemeProvider theme={theme}>
-		<BrowserRouter>
-			<Switch>
-				<Route exact path="/" render={home} />
-				<Route path="/About" component={About} />
-				<Route path="/Post/:Slug" render={post} />
-				<Route path="/Blog" render={blog} />
-				<Route path="/Projects" render={projects} />
-				<Route path="/Manage/About" component={ManageAbout} />
-			</Switch>
-		</BrowserRouter>
-	</ThemeProvider>
-);
+class Router extends React.Component {
+	render() {
+		return (
+			<ThemeProvider theme={theme}>
+				<BrowserRouter>
+					<Switch>
+						<Route exact path="/" render={home} />
+						<Route path="/About" component={About} />
+						<Route path="/Post/:Slug" render={post} />
+						<Route path="/Blog" render={blog} />
+						<Route path="/Projects" render={projects} />
+						<Route path="/Manage/About" component={ManageAbout} />
+					</Switch>
+				</BrowserRouter>
+			</ThemeProvider>
+		);
+	}
+}
 
 export default Router;
