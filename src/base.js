bd597567bdcf17672fb2a631e64426298d9ef35d
Marla Foreman <marla294@gmail.com>
2022-02-24 12:40:13 -0600

    Create base.js

---
+++ src/base.js
@@ -0,0 +1,19 @@
+import Rebase from 're-base';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBvCkfTPwKwpMCWypHHSFburuiKfUs_IL8",
+    authDomain: "marlaforeman-website.firebaseapp.com",
+    databaseURL: "https://marlaforeman-website-default-rtdb.firebaseio.com",
+    projectId: "marlaforeman-website",
+    storageBucket: "marlaforeman-website.appspot.com",
+    messagingSenderId: "580044535945",
+    appId: "1:580044535945:web:e86829af311bfc8b29ae58"
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+export { firebaseApp };
+
+export default base;
