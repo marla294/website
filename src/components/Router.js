cea6ab895732019e8c358dfb7acec79a90e1ebf1
Marla Foreman <marla294@gmail.com>
2022-03-04 17:48:25 -0600

    WIP - convert router to use hooks

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./GlobalStyles";
@@ -14,29 +14,67 @@ import base, { firebaseStorage } from '../base';
 
 const Router = (props) => {
 	const [about, setAbout] = useState({});
+	const [posts, setPosts] = useState([]);
+	const [aboutImageUrl, setAboutImageUrl] = useState("");
+
+	useEffect(() => {
+		const aboutRef = base.syncState('/about', {
+			context: {
+				setState: (stateObject) => {
+					if (stateObject.about) {
+						setAbout({ ...stateObject.about });
+					}
+				},
+			},
+			state: 'about'
+		});
+
+		const postsRef = base.syncState('/posts', {
+			context: {
+				setState: (stateObject) => {
+					if (stateObject.posts) {
+						setPosts([...posts]);
+					}
+				},
+			},
+			state: 'posts'
+		});
+
+		const aboutImageRef = firebaseStorage.ref().child('About.jpg');
+
+		aboutImageRef.getDownloadURL().then(url => {
+			setAboutImageUrl(url);
+		});
+		
+		return () => {
+			base.removeBinding(aboutRef);
+			base.removeBinding(postsRef);
+			base.removeBinding(aboutImageRef);
+		};
+	}, []);
 
 	return (
 		<ThemeProvider theme={theme}>
 			<BrowserRouter>
 				<Switch>
 					<Route exact path="/" render={(props) => {
-						return <Home posts={this.state.posts} {...props} />;
+						return <Home posts={posts} {...props} />;
 					}} />
 					<Route path="/About" render={(props) => {
 						return <About 
 							about={about} 
-							aboutImageUrl={this.state.aboutImageUrl}
+							aboutImageUrl={aboutImageUrl}
 							{...props} 
 						/>
 					}} />
 					<Route path="/Post/:Slug" render={(props) => {
-						return <Post posts={this.state.posts} {...props} />;
+						return <Post posts={posts} {...props} />;
 					}} />
 					<Route path="/Blog" render={(props) => {
-						return <Blog posts={this.state.posts} {...props} />;
+						return <Blog posts={posts} {...props} />;
 					}} />
 					<Route path="/Projects" render={(props) => {
-						return <Projects posts={this.state.posts} {...props} />;
+						return <Projects posts={posts} {...props} />;
 					}} />
 					<Route path="/Manage/About" render={() => {
 						return <ManageAbout 
@@ -70,24 +108,24 @@ export default Router;
 // 		posts: [],
 // 	};
 
-// 	componentDidMount() {
-// 		this.ref = base.syncState('about', {
-// 			context: this,
-// 			state: 'about'
-// 		});
+	// componentDidMount() {
+	// 	this.ref = base.syncState('about', {
+	// 		context: this,
+	// 		state: 'about'
+	// 	});
 
-// 		this.postsRef = base.syncState('posts', {
-// 			context: this,
-// 			state: 'posts'
-// 		});
+	// 	this.postsRef = base.syncState('posts', {
+	// 		context: this,
+	// 		state: 'posts'
+	// 	});
 
-// 		this.storageRef = firebaseStorage.ref();
-// 		this.aboutImageRef = this.storageRef.child('About.jpg');
+	// 	this.storageRef = firebaseStorage.ref();
+	// 	this.aboutImageRef = this.storageRef.child('About.jpg');
 
-// 		this.aboutImageRef.getDownloadURL().then(url => {
-// 			this.setState({aboutImageUrl: url})
-// 		});
-// 	}
+	// 	this.aboutImageRef.getDownloadURL().then(url => {
+	// 		this.setState({aboutImageUrl: url})
+	// 	});
+	// }
 
 // 	componentWillUnmount() {
 //         base.removeBinding(this.ref);
