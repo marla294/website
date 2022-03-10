4b4e0b8eca3c1da5999414a22372b1f4e6dbb848
Marla Foreman <marla294@gmail.com>
2022-03-10 15:48:22 -0600

    Cleanup on add post form

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
@@ -11,97 +11,96 @@ import Submit from './Styles/Submit';
 import useForm from '../lib/useForm';
 
 const AddPost = (props) => {
-    const { 
-        inputs, 
-        handleChange,
-        handleDateChange,
-        handleCategoryAddition,
-        handleCategoryDeletion,
-        handleEditorChange,
-    } = useForm({
-        date: new Date(),
-        title: '',
-        status: 'draft',
-        categories: [],
-        content: '',
-        headerImage: null,
-    });
+  const { 
+    inputs, 
+    handleChange,
+    handleDateChange,
+    handleCategoryAddition,
+    handleCategoryDeletion,
+    handleEditorChange,
+  } = useForm({
+    date: new Date(),
+    title: '',
+    status: 'draft',
+    categories: [],
+    content: '',
+    headerImage: null,
+  });
 
-    const addNewPost = e => {
-        e.preventDefault();
-        const post = {
-            date: inputs.date.toDateString(),
-            title: inputs.title,
-            status: inputs.status,
-            categories: inputs.categories,
-            content: inputs.content,
-            id: `Post_${new Date().getTime()}`
-        };
-
-        props.addNewPost(post);
-        props.uploadPostHeader(inputs.headerImage, post.id);
+  const addNewPost = e => {
+    e.preventDefault();
+    const post = {
+      date: inputs.date.toDateString(),
+      title: inputs.title,
+      status: inputs.status,
+      categories: inputs.categories,
+      content: inputs.content,
+      id: `Post_${new Date().getTime()}`
     };
 
+    props.addNewPost(post);
+    props.uploadPostHeader(inputs.headerImage, post.id);
+  };
+
     return (
-        <React.Fragment>
+      <React.Fragment>
         <Wrapper>
-            <ManageContent>
-                <h1>Add New Blog Post</h1>
-                <ManageFormStyles onSubmit={addNewPost}>
-                    <label>Date:</label>
-                    <div>
-                        <DatePicker 
-                            selected={inputs.date}
-                            onChange={handleDateChange}
-                        />
-                    </div>
-                    <label>Image:</label>
-                    <input 
-                        name="headerImage"
-                        type="file" 
-                        onChange={handleChange} 
-                    />
-                    <label>Title:</label>
-                    <input 
-                        type="text"
-                        name="title"
-                        onChange={handleChange}
-                        value={inputs.title}
-                    />
-                    <label>Status:</label>
-                    <select 
-                        type="text" 
-                        name="status" 
-                        onChange={handleChange} 
-                        value={inputs.status}
-                    >
-                        <option value="draft">Draft</option>
-                        <option value="public">Public</option>
-                        <option value="archive">Archive</option>
-                    </select>
-                    <label>Categories:</label>
-                    <div>
-                        <ReactTags 
-                            tags={inputs.categories}
-                            delimiters={[188, 13]}
-                            handleAddition={handleCategoryAddition}
-                            handleDelete={handleCategoryDeletion}
-                        />
-                    </div>
-                    <label>Post Content:</label>
-                    <Editor
-                        apiKey="6iwtqmlk62i53rbkbzwap5z37phnitxrj9fsdyy9ri2k2ykj"
-                        value={inputs.content}
-                        onEditorChange={handleEditorChange}
-                    />
-                    <Submit type="submit">Submit</Submit>
-                </ManageFormStyles>
-            </ManageContent>
+          <ManageContent>
+            <h1>Add New Blog Post</h1>
+            <ManageFormStyles onSubmit={addNewPost}>
+              <label>Date:</label>
+              <div>
+                  <DatePicker 
+                      selected={inputs.date}
+                      onChange={handleDateChange}
+                  />
+              </div>
+              <label>Image:</label>
+              <input 
+                  name="headerImage"
+                  type="file" 
+                  onChange={handleChange} 
+              />
+              <label>Title:</label>
+              <input 
+                  type="text"
+                  name="title"
+                  onChange={handleChange}
+                  value={inputs.title}
+              />
+              <label>Status:</label>
+              <select 
+                  type="text" 
+                  name="status" 
+                  onChange={handleChange} 
+                  value={inputs.status}
+              >
+                  <option value="draft">Draft</option>
+                  <option value="public">Public</option>
+                  <option value="archive">Archive</option>
+              </select>
+              <label>Categories:</label>
+              <div>
+                  <ReactTags 
+                      tags={inputs.categories}
+                      delimiters={[188, 13]}
+                      handleAddition={handleCategoryAddition}
+                      handleDelete={handleCategoryDeletion}
+                  />
+              </div>
+              <label>Post Content:</label>
+              <Editor
+                  apiKey="6iwtqmlk62i53rbkbzwap5z37phnitxrj9fsdyy9ri2k2ykj"
+                  value={inputs.content}
+                  onEditorChange={handleEditorChange}
+              />
+              <Submit type="submit">Submit</Submit>
+            </ManageFormStyles>
+          </ManageContent>
         </Wrapper>
         <GlobalStyle />
-    </React.Fragment>
+      </React.Fragment>
     );
-
 }
 
 export default AddPost;
