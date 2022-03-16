2f8ec8bbec48e17383d60ea99d31a8ba7b442090
Marla Foreman <marla294@gmail.com>
2022-03-16 15:06:58 -0500

    Styling login component nicer

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Wrapper from './Styles/Wrapper';
+import ManageContentStyles from './Styles/ManageContentStyles';
+import { GlobalStyle } from './GlobalStyles';
 
 const Login = ({authenticate}) => (
-    <nav className="login">
+  <React.Fragment>
+    <Wrapper>
+      <ManageContentStyles>
         <h2>Login</h2>
         <p>Sign in to manage your website, Marla E. Foreman</p>
         <button 
-        className="github" 
-        onClick={() => authenticate('Github')}
-        >Log In With Github</button>
-    </nav>
+          className="github" 
+          onClick={() => authenticate('Github')}
+        >
+          Log In With Github
+        </button>
+      </ManageContentStyles>
+    </Wrapper>
+    <GlobalStyle />
+  </React.Fragment>
 );
 
 Login.propTypes = {
