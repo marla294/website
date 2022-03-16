a9e7048cafd2cf006a8fc59bedf3952292de8fec
Marla Foreman <marla294@gmail.com>
2022-03-16 14:45:19 -0500

    WIP - auth hook

--- src/components/ManageAbout.js
+++ src/components/ManageAbout.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Editor } from '@tinymce/tinymce-react';
-import firebase from 'firebase';
 import { GlobalStyle } from "./GlobalStyles";
 import Wrapper from './Styles/Wrapper';
 import ManageContentStyles from './Styles/ManageContentStyles';
 import Submit from './Styles/Submit';
 import ManageFormStyles from "./Styles/ManageFormStyles";
 import Login from './Login';
-import base, { firebaseApp } from '../base';
 import useForm from '../lib/useForm';
+import useAuth from '../lib/useAuth';
 
 const ManageAbout = (props) => {
   const { 
@@ -23,15 +22,11 @@ const ManageAbout = (props) => {
     aboutImage: null,
   });
 
-  const [auth, setAuth] = useState({});
-
-  useEffect(() => {
-    firebase.auth().onIdTokenChanged(user => {
-      if (user) {
-        authHandler({user});
-      }
-    });
-  }, []);
+  const {
+    auth,
+    authenticate,
+    logout,
+  } = useAuth({});
 
   useEffect(() => {
     setInputs({
@@ -44,33 +39,6 @@ const ManageAbout = (props) => {
     e.preventDefault();
     props.updateAbout({ blurb: inputs.content });
     props.uploadAboutImage(inputs.aboutImage);
-  };
-
-  const authHandler = async (authData) => {
-    const owner = await base.fetch('owner', {context: this});
-
-    setAuth({
-      ...auth,
-      uid: authData.user.uid,
-      owner: owner || authData.user.uid,
-    });
-  };
-
-  const authenticate = () => {
-    const authProvider = new firebase.auth['GithubAuthProvider']();
-
-    firebaseApp
-      .auth()
-      .signInWithPopup(authProvider)
-      .then(authHandler);
-  };
-
-  const logout = async () => {
-    await firebase.auth().signOut();
-    setAuth({
-      ...auth,
-      uid: null,
-    });
   };
 
   if (!auth.uid) {
--- src/lib/useAuth.js
+++ src/lib/useAuth.js
@@ -1,10 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import firebase from 'firebase';
+import base, { firebaseApp } from '../base';
 
 export default function useAuth(initial = {}) {
   const [auth, setAuth] = useState(initial);
 
+  useEffect(() => {
+    firebase.auth().onIdTokenChanged(user => {
+      if (user) {
+        authHandler({user});
+      }
+    });
+  }, []);
+
+  const authHandler = async (authData) => {
+    const owner = await base.fetch('owner', {context: this});
+
+    setAuth({
+      ...auth,
+      uid: authData.user.uid,
+      owner: owner || authData.user.uid,
+    });
+  };
+
+  const authenticate = () => {
+    const authProvider = new firebase.auth['GithubAuthProvider']();
+
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(authHandler);
+  };
+
+  const logout = async () => {
+    await firebase.auth().signOut();
+    setAuth({
+      ...auth,
+      uid: null,
+    });
+  };
+
   return {
     auth,
-    setAuth,
+    authenticate,
+    logout,
   }
 }
